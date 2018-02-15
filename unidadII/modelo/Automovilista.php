95d893aa17961a1fa6dddefb2287820dd846c195
Erick J. M. Torres <ejmt1901@gmail.com>
2018-02-15 16:49:52 -0600

    Update Automovilista.php

--- unidadII/modelo/Automovilista.php
+++ unidadII/modelo/Automovilista.php
@@ -8,7 +8,7 @@ namespace modelo;
  *                                       *
  *****************************************/
 
-class Automovilista
+class Automovilista extends Docente
 {
     private $noTarjetaAcceso;
     private $tarjetaConducir;
@@ -62,6 +62,64 @@ class Automovilista
         $this->automovil = $automovil;
     }
 
+    
+    
+}
+
+class Automovilista2 extends Estudiante
+{
+    private $noTarjetaAcceso;
+    private $tarjetaConducir;
+    private $aclaracion = Array();
+    private $automovil = Array();
+    
+    function __construct(){
+        
+    }
+    /**
+     * @return mixed
+     */
+    public function getNoTarjetaAcceso()
+    {
+        return $this->noTarjetaAcceso;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getTarjetaConducir()
+    {
+        return $this->tarjetaConducir;
+    }
+    
+    /**
+     * @param mixed $noTarjetaAcceso
+     */
+    public function setNoTarjetaAcceso($noTarjetaAcceso)
+    {
+        $this->noTarjetaAcceso = $noTarjetaAcceso;
+    }
+    
+    /**
+     * @param mixed $tarjetaConducir
+     */
+    public function setTarjetaConducir($tarjetaConducir)
+    {
+        $this->tarjetaConducir = $tarjetaConducir;
+    }
+    
+    public function addAclaracion(Aclaracion $aclaracion){
+        $this->aclaracion = $aclaracion;
+    }
+    
+    /**
+     * @param multitype: $automovil
+     */
+    public function addAutomovil(Automovil $automovil)
+    {
+        $this->automovil = $automovil;
+    }
+    
     
     
 }
